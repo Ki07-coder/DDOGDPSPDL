6d97a65807f4ccaed54ea5acbd77e90e1d11db73
Ki07-coder <101982256+Ki07-coder@users.noreply.github.com>
2026-01-30 16:15:42 +0100

    changelog fix

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -7,7 +7,7 @@ export default {
             <div class="change">
                 <h2>30/01/2026</h2>
                 <p>Added changelog tab. Older changelogs are not available.</p>
-                <p>Below will show all future changes to list rankings aswell as changes to the list format or rules.</p>
+                <p>This page will show all future changes to list rankings aswell as changes to the list format or rules.</p>
             </div>
         </div>
     `,
