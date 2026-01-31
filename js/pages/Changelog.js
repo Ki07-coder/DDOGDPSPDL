837c7050480f8f6f856e4e82b7d8e5db34420791
Ki07-coder <101982256+Ki07-coder@users.noreply.github.com>
2026-01-31 21:55:22 +0100

    Removed roulette for rework

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -6,6 +6,9 @@ export default {
         <div v-else class="page-changelog">
             <div class="change">
                 <h2>31/01/2026</h2>
+                <h3>Major changes</h3>
+                <p>The roulette tab has been temporarily removed, I will try to rework it to work with platformer levels as percentage doesn't quite make sense.</p>
+                <h3>Placements</h3>
                 <p>"The Agony Tower" has been placed at #7, below "Two Steps Ahead".</p>
                 <p>This pushes "7" out of the top 10.</p>
             </div>
