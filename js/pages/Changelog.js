52f6280bd323a34c48fc8056af825bda85e57d51
Ki07-coder <101982256+Ki07-coder@users.noreply.github.com>
2026-04-02 15:35:22 +0200

    changelog fix

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -5,6 +5,11 @@ export default {
         </main>
         <div v-else class="page-changelog">
             <div class="change">
+                <h2>01/04/2026</h2>
+                <p>FIRE IN THE HOLE!</p>
+                <p>:)</p>
+            </div>
+            <div class="change">
                 <h2>27/03/2026</h2>
                 <p>"IWannaDanceDance" has been moved #11 -> #8, pushing "The Agony Tower" out of the top 10.</p>
                 <p>"shock therapy" has been placed at #53.</p>
